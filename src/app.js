78faa9c30fd2edd57125aceae157f75027c1a489
candacemonster <112183197+candacemonster@users.noreply.github.com>
2022-11-16 09:33:55 -0800

    Updated css (some js) for layout of app

--- src/app.js
+++ src/app.js
@@ -24,16 +24,7 @@ function formatDate(timestamp) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tueday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   return days[day];
 }
